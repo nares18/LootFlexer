5ad813e78948c5e389fdf49f28099add2f5123e8
nares18 <nares1818@gmail.com>
2026-08-12 18:22:12 -0400

    Adjust Config Group name

--- src/main/java/com/lootflexer/LootFlexerConfig.java
+++ src/main/java/com/lootflexer/LootFlexerConfig.java
@@ -5,7 +5,7 @@ import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.Range;
 
-@ConfigGroup("example")
+@ConfigGroup("Loot Flexer Settings")
 public interface LootFlexerConfig extends Config
 {
 	@ConfigItem(
@@ -23,5 +23,5 @@ public interface LootFlexerConfig extends Config
 	@Range(min = 1000000, max = Integer.MAX_VALUE)
 	default int getMinValue() {
 		return 1000000;
-	};
+	}
 }
